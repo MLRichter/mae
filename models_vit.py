77aa214ecba0955fa65a3dadd2584009b879b597
MLRichter <matrichter@uos.de>
2023-09-16 13:54:15 -0400

    cifar10

--- models_vit.py
+++ models_vit.py
@@ -59,6 +59,13 @@ def vit_base_patch2(**kwargs):
         norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
+
+def vit_base_patch4(**kwargs):
+    model = VisionTransformer(
+        patch_size=4, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
 def vit_base_patch8(**kwargs):
     model = VisionTransformer(
         patch_size=8, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
