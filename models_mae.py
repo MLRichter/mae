0b21be8d66ff1a2fb26e6a6e5459aaa7d7c59a67
MLRichter <matrichter@uos.de>
2023-09-18 10:28:42 -0400

    cifar10

--- models_mae.py
+++ models_mae.py
@@ -238,7 +238,7 @@ def mae_vit_base_patch4_dec512d8b(**kwargs):
 
 def mae_vit_base_patch1_dec512d8b(**kwargs):
     model = MaskedAutoencoderViT(
-        patch_size=2, embed_dim=768, depth=12, num_heads=12,
+        patch_size=1, embed_dim=768, depth=12, num_heads=12,
         decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
         mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
